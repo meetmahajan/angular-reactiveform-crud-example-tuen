8848b5b089849ee1ca05ca8c7b4c4ec2209465ca
meetmahajan <monoarchos@gmail.com>
2019-09-17 13:34:00 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,42 @@ export class AppComponent {
   get ticketArray() {
     return this.controls.tickets as FormArray;
   }
+  onChangeTickets(e) {
+    const numberOfTickets = e.target.value || 0;
+    if (this.ticketArray.length < numberOfTickets) {
+      for (let i = this.ticketArray.length; i < numberOfTickets; i++) {
+        this.ticketArray.push(this.formBuilder.group({
+          name: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]]
+        }));
+      }
+    } else {
+      for (let i = this.ticketArray.length; i >= numberOfTickets; i--) {
+        this.ticketArray.removeAt(i);
+      }
+    }
+  }
 
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.dynamicForm.invalid) {
+      return;
+    }
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.dynamicForm.value, null, 4));
+  }
+
+  onReset() {
+    // reset whole form back to initial state
+    this.submitted = false;
+    this.dynamicForm.reset();
+    this.ticketArray.clear();
+  }
+
+  onClear() {
+    // clear errors and reset ticket fields
+    this.submitted = false;
+    this.ticketArray.reset();
+  }
 }
